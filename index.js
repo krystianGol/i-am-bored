682664742ca0a848ebef24b7ccf5b6b423fbd7a0
Krystian <krystianrafalgolda@gmail.com>
2024-03-04 14:32:14 +0100

    write the style of the website

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
