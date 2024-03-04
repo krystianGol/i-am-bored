7b0a9f299ea0799c69892fbdc2e8fc5a17eb9cc3
Krystian <krystianrafalgolda@gmail.com>
2024-03-04 14:41:27 +0100

    initialize get from api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
@@ -7,9 +8,20 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get("https://bored-api.appbrewery.com/random");
+        const result = response.data;
+        console.log(result);
+        res.render("index.ejs", { data: result });
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+            error: error.message,
+        });
+    }
 })
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
